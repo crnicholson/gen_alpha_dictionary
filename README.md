221da038c4d4d2755349395090ad40ff653c46fc
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-12-10 20:00:00 -0500

    File structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust CLI
+
+This is a Rust CLI project. I'm learning Rust and making a bunch of projects along the way that will eventually culminate with a CLI. 
---
+++ projects/pig_latin/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = "testing";
+    let output = pig_latin_converter(&input);
+    println!("Original word: {input}, converted to Pig Latin: {output}.");
+}
+
+fn pig_latin_converter(input: &str) -> &str {
+    "testing"
+}
