e8e41c11f6c83c90c2964bb3b51ef4edb0bd9312
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-12-29 00:00:39 +0000

    Modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
-use std::{fs, error::Error};
-use serde_json::Value;
+pub mod dictionary {
+    use std::{fs, error::Error};
+    use serde_json::Value;
 
-pub fn parse_args(args: &[String]) -> Result<String, &'static str> {
-    if args.len() < 2 {
-        return Err("Please enter the skibidi word you want defined.");
+    pub fn parse_args(args: &[String]) -> Result<String, &'static str> {
+        if args.len() < 2 {
+            return Err("Please enter the skibidi word you want defined.");
+        }
+
+        Ok(args[1].clone().to_lowercase().replace('"', ""))
     }
 
-    Ok(args[1].clone().to_lowercase().replace('"', ""))
-}
+    pub fn parse_json(path: &str) -> Result<Value, Box<dyn Error>> {
+        let json: String = fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&json)?)
+    }
 
-pub fn parse_json(path: &str) -> Result<Value, Box<dyn Error>> {
-    let json: String = fs::read_to_string(path)?;
-    Ok(serde_json::from_str(&json)?)
-}
-
-pub fn define_word<'a>(dict: &'a Value, key: &'a str) -> Result<&'a str, &'static str> {
-    match dict[key].as_str() {
-        Some(value) => Ok(value),
-        None => Err("Uh-oh my skibidi rizzler, that Ohio word is not in my sigma dictionary.")
+    pub fn define_word<'a>(dict: &'a Value, key: &'a str) -> Result<&'a str, &'static str> {
+        match dict[key].as_str() {
+            Some(value) => Ok(value),
+            None => Err("Uh-oh my skibidi rizzler, that Ohio word is not in my sigma dictionary.")
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::{env, process};
 use serde_json::Value;
+use gen_alpha_dictionary::dictionary;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let word = gen_alpha_dictionary::parse_args(&args).unwrap_or_else(|err| {
+    let word = dictionary::parse_args(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    let dict: Value = gen_alpha_dictionary::parse_json("dict.json").unwrap_or_else(|err| {
+    let dict: Value = dictionary::parse_json("dict.json").unwrap_or_else(|err| {
         println!("Problem parsing dictionary: {err}");
         process::exit(1);
     });
 
-    let result = gen_alpha_dictionary::define_word(&dict, &word).unwrap_or_else(|err | {
+    let result = dictionary::define_word(&dict, &word).unwrap_or_else(|err | {
         println!("{err}");
         process::exit(1);
     });
